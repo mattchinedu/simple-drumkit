7bb38b407700c6905d9e8df6639fb2d17f4dab9f
chukwu chinedu matthew <107567750+mattchinedu@users.noreply.github.com>
2022-08-29 19:26:48 +0100

    Add files via upload

---
+++ style.js
@@ -0,0 +1,8 @@
+
+var numberOfDrum=document.querySelectorAll(".drum").length;
+for(var i=i;i<numberOfDrum;i++){
+    document.querySelectorAll(".drum")[1].addEventListener("click",function(){
+        alert("i got click!");
+        });
+
+}
